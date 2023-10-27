e2edd64d0032c805ea4809a9fbb0c2a652e09502
sobaryton <solene.bary@gmail.com>
2023-10-27 16:35:21 +0100

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        'plugin:@typescript-eslint/recommended',
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        '@typescript-eslint'
+    ],
+    "rules": {
+        "consistent-return": 2,
+        "indent"           : [1, 4],
+        "no-else-return"   : 1,
+        "semi"             : [1, "always"],
+        "space-unary-ops"  : 2
+    }
+}
